1804e0e5635b2036e77ff3d384f0b81a6fd4be14
Lee Jun Seop <ppq23@naver.com>
2019-05-29 12:55:33 +0900

    15649 add comment

--- 15649.cpp
+++ 15649.cpp
@@ -1,24 +1,24 @@
 #include <stdio.h>
-#define MAX 9
+#define MAX 9 //n ,m <= 8
 
 int printlist(int,int,int[MAX],int);
 
 int main()
 {
 	int n, m;
-	int position = 1;
-	scanf("%d %d", &n, &m);
-	int list[MAX];
+	int position = 1; // start list position = 1
+	scanf("%d %d", &n, &m); // input n,m 
+	int list[MAX]; // 
 	printlist(n,m,list, position);
 
 	return 0;
 }
 
 
-int printlist(int n, int m, int list[MAX], int position)
+int printlist(int n, int m, int list[MAX], int position) // make & print number
 {
 	int include;
-	if(position > m)
+	if(position > m) // if makelist finish print list
 	{
 		for(int i = 1; i <= m; i++)
 		{
@@ -29,16 +29,16 @@ int printlist(int n, int m, int list[MAX], int position)
 	}
 	else
 	{
-		for(int i = 1; i <= n; i++)
+		for(int i = 1; i <= n; i++) // loop 1~n
 		{
 			include = 0;
 			for(int j = 1; j < position; j++)
-				if(list[j] == i)
+				if(list[j] == i) // compare previous number
 					include = 1;
-			if (include == 0)
+			if (include == 0) // If the numbers are not there before
 			{
-				list[position] = i;
-				printlist(n, m, list, position+1);
+				list[position] = i; // put list number
+				printlist(n, m, list, position+1); // find next number
 			}
 		}
 		return 0;
