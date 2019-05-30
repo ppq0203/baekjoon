b6d167579c4e092befdf8cd9db92094cad059e3f
js desktop <ppq23@naver.com>
2019-05-30 21:03:05 +0900

    15651 n&m.3

---
+++ 15651.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX 9 //n ,m <= 8
+
+int printlist(int,int,int[MAX],int);
+
+int main()
+{
+	int n, m;
+	int position = 1; // start list position = 1
+	scanf("%d %d", &n, &m); // input n,m 
+	int list[MAX]; // 
+	printlist(n,m,list, position);
+
+	return 0;
+}
+
+
+int printlist(int n, int m, int list[MAX], int position) // make & print number
+{
+	if(position > m) // if makelist finish print list
+	{
+		for(int i = 1; i <= m; i++)
+		{
+			printf("%d ", list[i]);
+		}
+		printf("\n");
+		return 0;
+	}
+	else
+	{
+		for(int i = 1; i <= n; i++) // loop 1~n
+		{
+			list[position] = i; // put list number
+			printlist(n, m, list, position+1); // find next number
+		}
+		return 0;
+	}
+}
