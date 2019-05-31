b492d9bc7c3e89d9e455f4b1218931738a0cc8c8
js desktop <ppq23@naver.com>
2019-05-31 22:55:33 +0900

    15654 n&m.5

---
+++ 15654.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 9 //n ,m <= 8
+
+int func_compare(const void * first, const void * second);
+int makelist(int,int,int[MAX],int,int[MAX]);
+
+int main()
+{
+	int n, m;
+	int position = 1; // start list position = 1
+	int list[MAX] = {0};
+	int numlist[MAX] = {0};
+	scanf("%d %d", &n, &m); // input n,m 
+	for(int i = 1; i <= n; i++)
+		scanf("%d", numlist+i); // input the number of n
+	qsort((int *)numlist, n+1, sizeof(int), func_compare); //sort number list
+	makelist(n,m,list, position, numlist);
+
+	return 0;
+}
+
+int func_compare(const void * first, const void * second) 
+{
+	if (*(int*)first > *(int *)second) //ascending order
+		 return 1;
+	else
+		return 0; 
+}
+
+void printlist(int[MAX],int,int[MAX]);
+
+int makelist(int n, int m, int list[MAX], int position, int numlist[MAX]) // make & print number
+{
+	int include;
+	if(position > m) // if makelist finish print list
+	{
+		printlist(list, m, numlist);
+		return 0;
+	}
+	else
+	{
+		for(int i = 1; i <= n; i++) // loop 1~n
+		{
+			include = 0;
+			for(int j = 1; j < position; j++)
+				if(list[j] == i) // compare previous number
+					include = 1;
+			if (include == 0) // If the numbers are not there before
+			{
+				list[position] = i; // put list number
+				makelist(n, m, list, position+1, numlist); // find next number
+			}
+		}
+		return 0;
+	}
+}
+
+void printlist(int list[MAX], int m, int numlist[MAX])
+{
+	for(int i = 1; i <= m; i++)
+	{
+		printf("%d ", numlist[list[i]]);
+	}
+	printf("\n");
+}
