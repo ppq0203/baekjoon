e796cd39fb30b4eca7ea67cce565b63d9745166d
js desktop <ppq23@naver.com>
2019-06-20 22:33:41 +0900

    1436fix

--- 1436.cpp
+++ 1436.cpp
@@ -7,13 +7,12 @@ int main(int argc, char const *argv[])
 	int n;
 	char movienum[MAX];
 	char key[4] = "666";
-
 	scanf("%d", &n); // input
 	int findnum = 0; // num contain 666 +count
 	for (int num = 666; findnum < n; num++) // first title is 666
 	{
-		sprintf(movienum, "%d", num);
-		if (strstr(movienum, key) != NULL)
+		sprintf(movienum, "%d", num); // int to string
+		if (strstr(movienum, key) != NULL) // movienum contain key
 			findnum++;
 	}
 	printf("%s", movienum);
